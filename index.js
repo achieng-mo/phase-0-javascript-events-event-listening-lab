93bbfa809469551529f899f4a189dbab569da5eb
Morine Achieng <m.achieng96@gmail.com>
2023-06-24 05:25:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+//Get DOM element 'input' and reference it to a variable (input)
+//Use .addEventListener method with 2 arguments (event name-click, arrow function)
+//Call the function addEventListener
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click',() => alert('I was clicked!'));
 }
+
+addingEventListener()
+
+
